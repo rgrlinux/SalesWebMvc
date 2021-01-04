5e183d08b11d718b2178dd74b83c02631581a48f
rgrlinux <rgrlinux@gmail.com>
2021-01-04 00:49:19 -0300

    Implement seles service

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+    }
+}
