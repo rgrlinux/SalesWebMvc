e27ab51146cbe92ca8e027051c8268b530894848
rgrlinux <rgrlinux@gmail.com>
2021-01-08 22:52:24 -0300

    Update Seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DbConcurrentyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrentyException : ApplicationException
+    {
+        public DbConcurrentyException(string message) : base(message)
+        {
+
+        }
+    }
+}
